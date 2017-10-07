36b43544b1a40bdcd8b699788c2aca42580df245
TsengMingYu <qazwsx9006@hotmail.com>
2017-10-07 12:30:04 +0800

    use koa-logger

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const Koa = require('koa');
+const logger = require('koa-logger');
+
 const app = new Koa();
 
-app.use(async(ctx,next) => {
-  const start = Date.now();
-  await next();
-  const ms = Date.now() - start;
-  console.log(`${ctx.method} ${ctx.url} - ${ms} ms`)
-});
+// app.use(async(ctx,next) => {
+//   const start = Date.now();
+//   await next();
+//   const ms = Date.now() - start;
+//   console.log(`${ctx.method} ${ctx.url} - ${ms} ms`)
+// });
+app.use(logger());
 
 app.use(async function(ctx) {
     ctx.body = 'Hello Koa2';
 });
+
 
 app.listen(3001);
