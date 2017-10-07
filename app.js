ef2b00b7d7d7b6f3c526230ea6ebb59693949fa8
TsengMingYu <qazwsx9006@hotmail.com>
2017-10-07 12:29:00 +0800

    koa2 log response info

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const Koa = require('koa');
-
 const app = new Koa();
+
+app.use(async(ctx,next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms} ms`)
+});
 
 app.use(async function(ctx) {
     ctx.body = 'Hello Koa2';
