189ddd8677d27d23b3a8ea433e973ebd51a1bc64
TsengMingYu <qazwsx9006@hotmail.com>
2017-10-07 12:40:12 +0800

    use koa-bodyparser to Parse POST params

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const logger = require('koa-logger');
+const bodyParser = require('koa-bodyparser');
 
 const app = new Koa();
 const router = new Router();
@@ -20,8 +21,29 @@ router.get('/about', async(ctx) => {
     ctx.body = 'About Me';
 });
 
+// login page (GET)
+router.get('/login', async(ctx) => {
+    ctx.body = `
+    <form method="POST" action="/login">
+        <label>UserName</label>
+        <input name="usr" /><br/>
+        <button type="submit">submit</button>
+      </form>
+    `;
+});
+// login page (POST)
+router.post('/login', async(ctx) => {
+    let usr = ctx.request.body.usr;
+    // console.log(ctx.request.header.host)
+    // console.log('====')
+    // console.log(ctx.request.body)
+    // console.log(ctx.request.ip)
+    ctx.body = `<p>Welocome,${usr}!</p>`;
+});
 
 app.use(logger());
+
+app.use(bodyParser());
 
 app.use(router.routes());
 
